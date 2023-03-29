cbfb1e09b809da1fe8bb2618c6af32eb8c6c9d57
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-03-29 16:42:43 -0300

    code refactor

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
 let buttons = document.querySelectorAll(".button");
-let numberBtns = document.querySelectorAll("[type=number]");
 let display = document.querySelector("#display");
 let calculator = document.querySelector(".calculadora");
 let historic = document.querySelector(".previousOperation");
+
+function changeDisplayContent(content, option) {
+  if (option == "add") {
+    display.textContent += content;
+  } else if (option == "keep") {
+    display.textContent = content;
+  }
+}
+
+function cleanHistoric() {
+  historic.textContent = "";
+}
+
+// limpa datasets
+function resetDatasets() {
+  calculator.dataset.firstValue = "";
+  calculator.dataset.operator = "";
+  calculator.dataset.previousKeyType = "";
+}
 
 function calcular(n1, operator, n2) {
   let resultado = "";
@@ -21,113 +39,105 @@ function calcular(n1, operator, n2) {
   return resultado;
 }
 
-buttons.forEach((button) => {
+buttons.forEach((button) => {  
+
   button.addEventListener("click", (e) => {
+
     let target = e.target;
 
-    // pega do type operator do botão
-    let operatorType = target.dataset.type;
+    // pega do type do botão
+    let buttonType = target.dataset.type;
 
-    // pega o textContent do número clicado
-    let numValue = target.value;
+    // pega o valor/value do botao clicado
+    let buttonValue = target.value;
 
-    // pega o value do operador clicado
-    let operatorTargetValue = target.value;
+    if (buttonType == "number") {
 
-    if (operatorType == "number") {
       if (display.textContent == "0") {
-        display.textContent = numValue;
+        changeDisplayContent(buttonValue, "keep");
+
       } else if (calculator.dataset.previousKeyType == "equal") {
         // limpa e volta o display pra zero
-        display.textContent = "";
-        historic.textContent = "";
-
-        // limpa datasets
-        calculator.dataset.firstValue = "";
-        calculator.dataset.operator = "";
-        calculator.dataset.previousKeyType = "";
+        changeDisplayContent("", "keep");
+        cleanHistoric();
+        resetDatasets();
 
         // adiciona numero ao display
-        display.textContent += numValue;
+        changeDisplayContent(buttonValue, "add");
+
       } else {
-        display.textContent += numValue;
+        changeDisplayContent(buttonValue, "add");
       }
       calculator.dataset.previousKeyType = "number";
-    } else if (operatorType == "operator" && calculator.dataset.previousKeyType !== "operator" && calculator.dataset.operator == "") {
-      historic.textContent = "";
-      // pegar o numero atual exibido no display
-      // pegar numero antes de adicionar o simbolo do operador no display
+    } 
+    
+    else if (buttonType == "operator" && calculator.dataset.previousKeyType !== "operator" && calculator.dataset.operator == "") {
+
+      cleanHistoric();
+      // pegar o numero atual exibido no display antes de adicionar o simbolo do operador
       calculator.dataset.firstValue = display.textContent;
 
       // adiciona o value (simbolo) do tipo operador clicado no display
-      display.textContent += operatorTargetValue;
+      changeDisplayContent(buttonValue, "add");
 
       // pegar o simbolo do operador/value
-      calculator.dataset.operator = operatorTargetValue;
+      calculator.dataset.operator = buttonValue;
 
       calculator.dataset.previousKeyType = "operator";
       historic.textContent += `${calculator.dataset.firstValue} ${calculator.dataset.operator} `;
-    } else if (operatorType == "decimal") {
-      // adiciona ponto decimal
-      display.textContent += ".";
-    } else if (operatorType == "clean") {
-      // limpa e volta o display pra zero
-      display.textContent = "0";
-      historic.textContent = "";
+    } 
+    
+    else if (buttonType == "decimal") {
+      changeDisplayContent(".", "add");
+    }
 
-      // limpa datasets
-      calculator.dataset.firstValue = "";
-      calculator.dataset.operator = "";
-      calculator.dataset.previousKeyType = "";
-    } else if (operatorType == "backspace") {
-      // BACKSPACE
+    else if (buttonType == "clean") {
+      changeDisplayContent("0", "keep");
+      cleanHistoric();
+      resetDatasets();
+    } 
+    
+    else if (buttonType == "backspace") {
 
       let ultimoElementoDoDisplay = display.textContent.charAt(display.textContent.length - 1);
 
       // se ultimo elemento do display for igual ao operador, resetar dataset do operador
       if (ultimoElementoDoDisplay == calculator.dataset.operator) {
-        console.log(ultimoElementoDoDisplay);
         calculator.dataset.operator = "";
-
         historic.textContent = historic.textContent.slice(0, -2);
       }
 
       display.textContent = display.textContent.slice(0, -1);
-
       calculator.dataset.previousKeyType = "backspace";
-    } else if (operatorType == "equal") {
+    } 
+    
+    else if (buttonType == "equal") {
+
+      let firstValue = calculator.dataset.firstValue;
+      let operator = calculator.dataset.operator;
+
       // obter segundo valor do display
       let secondValue = "";
-
       let displayContent = display.textContent;
 
       for (let i = 0; i < displayContent.length; i++) {
-        if (displayContent.charAt(i) == calculator.dataset.operator) {
+        if (displayContent.charAt(i) == operator) {
           secondValue = displayContent.slice(i + 1);
           break;
         }
       }
 
-      console.log(secondValue);
-
-      // se a ultima tecla clicada for um operador ou o operador nao foi definido ou nao ha o segundo valor para realizar a operacao,
-      // se clicar no igual o display nao muda e nao calcula nada
+      // se a ultima tecla clicada for um operador ou o operador nao foi definido ou nao ha o segundo valor para realizar a operacao, o display nao muda e nao calcula nada
       if (calculator.dataset.previousKeyType == "operator" || calculator.dataset.operator == "" || secondValue == "") {
         display.textContent = display.textContent;
       }
-
-      //mostrar resultado
       else {
-        // limpar antes de mostrar resultado
-        display.textContent = "";
-
-        // calcula e mostra o resultado no display
-        let resultado = calcular(calculator.dataset.firstValue, calculator.dataset.operator, secondValue);
-        display.textContent = resultado;
+        //mostrar resultado
+        let result = calcular(firstValue, operator, secondValue);
+        changeDisplayContent(result, "keep");
 
         calculator.dataset.previousKeyType = "equal";
         calculator.dataset.operator = "";
-
         historic.textContent += `${secondValue} =`;
       }
     }
